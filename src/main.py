af61b4725e32f21400c2cf5cc7413d3029809e72
LewisWorton <c1014451@my.shu.ac.uk>
2025-03-17 18:56:15 +0000

    feat/ added directional movements

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------------- #
 #                                                                              #
 # 	Module:       main.py                                                      #
-# 	Author:       lewis                                                        #
+# 	Author:       Lewis                                                        #
 # 	Created:      3/17/2025, 4:44:36 PM                                        #
 # 	Description:  EXP project                                                  #
 #                                                                              #
@@ -11,9 +11,52 @@
 from vex import *
 
 # Brain should be defined by default
-brain=Brain()
 
-brain.screen.print("Hello EXP")
+def moveForward(motorLeft, motorRight):
+    motorLeft.spin(FORWARD)
+    motorRight.spin(REVERSE)
 
+def stopForward(motorLeft, motorRight):
+    motorLeft.stop()
+    motorRight.stop()
 
-        
+def moveBackward(motorLeft, motorRight):
+    motorLeft.spin(REVERSE)
+    motorRight.spin(FORWARD)
+
+def stopBackward(motorLeft, motorRight):
+    motorLeft.stop()
+    motorRight.stop()
+
+def turnLeft(motorLeft, motorRight, inertial):
+    curHeading = inertial.heading()
+    newHeading = curHeading + 90
+
+    while curHeading != newHeading:
+        curHeading = inertial.heading()
+        motorLeft.spin(REVERSE)
+        motorRight.spin(REVERSE)
+
+def turnRight(motorLeft, motorRight, inertial):
+    motorLeft.spin(FORWARD)
+    motorRight.spin(FORWARD)
+
+def main():
+    brain=Brain()
+    inertial = Inertial()
+    optical = Optical(Ports.PORT6)
+    distance = Distance(Ports.PORT3)
+    motorLeft = Motor(Ports.PORT1) # From bot perspective
+    motorRight = Motor(Ports.PORT2) # From bot perspective
+    
+    inertial.calibrate()
+
+    while True:
+        curHeading = inertial.heading()
+        brain.screen.print(curHeading)
+        brain.screen.next_row()
+        #if curDistance < 1000:
+            #moveForward(motorLeft, motorRight)
+        turnLeft(motorLeft, motorRight, inertial)
+
+main()
