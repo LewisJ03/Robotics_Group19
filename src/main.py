5c70ea839776448e1bef08485c6046e5ec814f51
LewisWorton <c1014451@my.shu.ac.uk>
2025-03-17 16:51:59 +0000

    initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       lewis                                                        #
+# 	Created:      3/17/2025, 4:44:36 PM                                        #
+# 	Description:  EXP project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello EXP")
+
+
+        
