3463a5fdf2fe69fc6ee093c8dfdf84ae816c723e
LewisWorton <c1014451@my.shu.ac.uk>
2025-04-10 12:48:20 +0100

    feat/ obstacle avoidance

--- src/main.py
+++ src/main.py
@@ -9,54 +9,91 @@
 
 # Library imports
 from vex import *
+import time
 
-# Brain should be defined by default
+class Bot:
+    def __init__ (self):
+        self.brain = Brain()
+        self.inertial = Inertial()
+        self.optical = Optical(Ports.PORT7)
+        self.distance = Distance(Ports.PORT5)
+        self.motorFrontLeft = Motor(Ports.PORT6) # From bot's perspective
+        self.motorBackLeft = Motor(Ports.PORT8)
+        self.motorFrontRight = Motor(Ports.PORT10)
+        self.motorBackRight = Motor(Ports.PORT9)
+    
+        self.baseSpeed = 30
+        self.turnSpeed = 15
+        self.tapeBrightnessThreshold = 30 # will needing adjusting based on testing
 
-def moveForward(motorLeft, motorRight):
-    motorLeft.spin(FORWARD)
-    motorRight.spin(REVERSE)
+        self.inertial.calibrate()
+        self.optical.set_light(100)
 
-def stopForward(motorLeft, motorRight):
-    motorLeft.stop()
-    motorRight.stop()
+    def moveForward(self):
+        self.motorFrontLeft.spin(FORWARD)
+        self.motorBackLeft.spin(FORWARD)
+        self.motorFrontRight.spin(REVERSE)
+        self.motorBackRight.spin(REVERSE)
 
-def moveBackward(motorLeft, motorRight):
-    motorLeft.spin(REVERSE)
-    motorRight.spin(FORWARD)
+    def moveBackward(self):
+        self.motorFrontLeft.spin(REVERSE)
+        self.motorBackLeft.spin(REVERSE)
+        self.motorFrontRight.spin(FORWARD)
+        self.motorBackRight.spin(FORWARD)
 
-def stopBackward(motorLeft, motorRight):
-    motorLeft.stop()
-    motorRight.stop()
+    def stop(self):
+        self.motorFrontLeft.stop()
+        self.motorBackLeft.stop()
+        self.motorFrontRight.stop()
+        self.motorBackRight.stop()
 
-def turnLeft(motorLeft, motorRight, inertial):
-    curHeading = inertial.heading()
-    newHeading = curHeading + 90
+    def turnLeft(self):
+        curHeading = self.inertial.heading()
+        newHeading = curHeading + 90
 
-    while curHeading != newHeading:
-        curHeading = inertial.heading()
-        motorLeft.spin(REVERSE)
-        motorRight.spin(REVERSE)
+        while curHeading != newHeading:
+            curHeading = self.inertial.heading()
+            self.motorFrontLeft.spin(REVERSE)
+            self.motorBackLeft.spin(REVERSE)
+            self.motorFrontRight.spin(REVERSE)
+            self.motorBackRight.spin(REVERSE)
 
-def turnRight(motorLeft, motorRight, inertial):
-    motorLeft.spin(FORWARD)
-    motorRight.spin(FORWARD)
+    def turnRight(self):
+        self.motorFrontLeft.spin(FORWARD)
+        self.motorBackLeft.spin(FORWARD)
+        self.motorFrontRight.spin(FORWARD)
+        self.motorBackRight.spin(FORWARD)
+
+def followPath(bot):
+    while True:
+        brightness = bot.optical.brightness() * 100 # Convert to percentage
+        detectedDistance = bot.distance.object_distance(MM)
+
+        if (detectedDistance > 280 or detectedDistance < 240):
+            bot.stop()
+            bot.moveBackward()
+            time.sleep(1)
+            bot.stop()
+            time.sleep(0.5)
+            bot.turnLeft()
+            bot.brain.screen.print("Obstacle detected!")
+            bot.brain.screen.next_row()
+            time.sleep(1)
+            continue
+        else:
+            bot.moveForward()
+
+        time.sleep(0.1)
 
 def main():
-    brain=Brain()
-    inertial = Inertial()
-    optical = Optical(Ports.PORT6)
-    distance = Distance(Ports.PORT3)
-    motorLeft = Motor(Ports.PORT1) # From bot perspective
-    motorRight = Motor(Ports.PORT2) # From bot perspective
-    
-    inertial.calibrate()
+    bot = Bot()
 
     while True:
-        curHeading = inertial.heading()
-        brain.screen.print(curHeading)
-        brain.screen.next_row()
+        curHeading = bot.optical.hue()
+        bot.brain.screen.print(curHeading)
+        bot.brain.screen.next_row()
         #if curDistance < 1000:
             #moveForward(motorLeft, motorRight)
-        turnLeft(motorLeft, motorRight, inertial)
+        followPath(bot)
 
 main()
