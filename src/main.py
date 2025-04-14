a758c9ce2013e92f654e7a3fbff747e36810e999
Lewis <70917466+LewisJ03@users.noreply.github.com>
2025-04-14 21:28:46 +0100

    tracking update & turning fixed

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ class Bot:
         self.motorBackRight = Motor(Ports.PORT9)
     
         self.baseSpeed = 30
-        self.turnSpeed = 15
+        self.turnSpeed = 5
         self.tapeBrightnessThreshold = 30 # will needing adjusting based on testing
 
         self.inertial.calibrate()
@@ -49,44 +49,93 @@ class Bot:
 
     def turnLeft(self):
         curHeading = self.inertial.heading()
-        newHeading = curHeading + 90
+        newHeading = round(curHeading - 90)
+        if newHeading<0:
+            newHeading+=360 
 
-        while curHeading != newHeading:
-            curHeading = self.inertial.heading()
-            self.motorFrontLeft.spin(REVERSE)
-            self.motorBackLeft.spin(REVERSE)
-            self.motorFrontRight.spin(REVERSE)
-            self.motorBackRight.spin(REVERSE)
-
+        self.motorFrontLeft.spin(REVERSE)
+        self.motorBackLeft.spin(REVERSE)
+        self.motorFrontRight.spin(REVERSE)
+        self.motorBackRight.spin(REVERSE)
+            
     def turnRight(self):
+        curHeading = self.inertial.heading()
+        newHeading = round(curHeading + 90)
+        if newHeading<360:
+            newHeading-=360 
+        
         self.motorFrontLeft.spin(FORWARD)
         self.motorBackLeft.spin(FORWARD)
         self.motorFrontRight.spin(FORWARD)
         self.motorBackRight.spin(FORWARD)
 
-def followPath(bot):
+def followPath(bot,trackingArr,coordX,coordY):
     while True:
         brightness = bot.optical.brightness() * 100 # Convert to percentage
         detectedDistance = bot.distance.object_distance(MM)
+        curHeading=bot.brain.inertial.heading()
+        if curHeading<45:
+            trackingArr.append(["-","-"])#adds y dimension to array
+        elif curHeading>45:
+            for i in range(len(trackingArr)):
+                trackingArr[(len(trackingArr))-i].append("-")#adds x dimension to array
 
-        if (detectedDistance > 280 or detectedDistance < 240):
+        if (detectedDistance > 280 or detectedDistance < 240):#stops bot if object in way or edge
             bot.stop()
             bot.moveBackward()
             time.sleep(1)
             bot.stop()
             time.sleep(0.5)
-            bot.turnLeft()
+            bot.turnRight()
+
             bot.brain.screen.print("Obstacle detected!")
             bot.brain.screen.next_row()
             time.sleep(1)
+            trackingArr[coordX][coordY]="!"
             continue
+        
+        elif (curHeading>315 or curHeading < 45) and trackingArr[coordX][coordY+1]=="!":#tracks for already taken
+            bot.stop()
+            time.sleep(0.5)
+            bot.turnRight()
+        elif (curHeading>45 and curHeading < 135) and trackingArr[coordX+1][coordY]=="!":
+            bot.stop()
+            time.sleep(0.5)
+            bot.turnRight()
+        elif (curHeading>135 and curHeading < 225) and trackingArr[coordX][coordY-1]=="!":
+            bot.stop()
+            time.sleep(0.5)
+            bot.turnRight()
+        elif (curHeading>225 or curHeading < 315) and trackingArr[coordX-1][coordY]=="!":
+            bot.stop()
+            time.sleep(0.5)
+            bot.turnRight()
+
         else:
             bot.moveForward()
+            trackingArr[coordX][coordY]="!"
 
+            if curHeading>315 or curHeading < 45:#sets x&y for next movement
+                coordY+=1
+            elif curHeading>45 and curHeading < 135:
+                coordX+=1
+            elif curHeading>135 and curHeading < 225:
+                coordY-=1
+            elif curHeading>225 and curHeading < 315:
+                coordX-=1
         time.sleep(0.1)
+        
+
+
+    
 
 def main():
     bot = Bot()
+
+    x ,y =2,2
+    trackArr=[['-'for i in range(x)]for j in range(y)]#initalise array for tracking
+    coordX=0
+    coordY=0
 
     while True:
         curHeading = bot.optical.hue()
@@ -94,6 +143,6 @@ def main():
         bot.brain.screen.next_row()
         #if curDistance < 1000:
             #moveForward(motorLeft, motorRight)
-        followPath(bot)
+        followPath(bot,trackArr,coordX,coordY)
 
 main()
